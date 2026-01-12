6ef55cc37823b9cc73cfac283b88a2d27b0545a1
Shivam-jha-17 <shivamjha7768896@gmail.com>
2026-01-12 14:25:01 +0000

    variables lecture over

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 114445
+let accountEmail = "shivam@google.com"
+var accountPassword = "123321"
+accountCity ="jaipur"
+let accountState;
+
+accountEmail = "shi@google.com"
+accountPassword = "321232"
+accountCity = "hydrebad"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
