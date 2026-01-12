9508b83def1112060f7a4a365e69a0cc524f48e1
Shivam-jha-17 <shivamjha7768896@gmail.com>
2026-01-12 15:02:09 +0000

    learn datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treal all js code as newer version
+//java script broswer ke v8 enigne pe run hota hai isliye alert command waha direct run ho jati hai but node.js mein iska alag method hai.
+// alert( 3 + 3 ) //we are using node.js, not browser
+
+//code readability should be high.
+//for documentation purpose go to these website 1. mdn 2. tc39
+let Name = "shivam" // string
+let age = 21; // numbers
+let isLoggedIn = false //boolean
+
+// primitive data types
+
+// number => 2^53
+// bigint
+// string = ""
+// boolean = true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
