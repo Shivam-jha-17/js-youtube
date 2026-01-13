635e4c623c4b63ff23cc86d981d148e81f92fe27
Shivam-jha-17 <shivamjha7768896@gmail.com>
2026-01-13 10:00:55 +0000

    i learn that how to compare data types with operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+// video no. 8 chai aur code.
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// kindly avoid these kind of camparion :)
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === this is called strict check both data types during comparison.
+
+console.log("2" === 2);
+
+
+
+
+
+
