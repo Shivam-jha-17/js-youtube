b14fcb714133cb8a5fe0e1de4ecd6a3b7f9db10e
Shivam-jha-17 <shivamjha7768896@gmail.com>
2026-01-25 11:21:01 +0000

    part 1 about objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,51 @@
+// we have two type of option to creating object
+// literal, constructor
+// singleton humesa create hota hai (while creating with constructor)
+// Object.create
+
+// but literals ka use karke agar objects declare kare toh singleton nahi banta hai . 
+
+// object literals
+
+const mySym = Symbol("key1")
+
+const Jsuser = {
+    name: "shivam", // but compiler name = "name" string ki trah leta hai 
+    "fullname": "shivam jha", 
+    [mySym]: "mykey1",
+    age: 21, 
+    location: "panipat",
+    email: "shivam@google.com",
+    isLoggedin: false,
+    lastLoginDays: ["monday", "saturday"]
+} 
+
+// console.log(Jsuser.email);
+// console.log(Jsuser["email"]);
+// console.log(Jsuser["fullname"]);
+// console.log(Jsuser[mySym]);
+
+// object ki value kaise access karte hain :- dot notation and bracket notation se
+// change kaise karte hai 
+
+Jsuser.email = "shivam@chatgpt.com"
+// Object.freeze(Jsuser) // freeze karne ke baad object ki value change nahi kar sakte hain
+Jsuser.email = "shivam@microsoft.com"
+// console.log(Jsuser);
+
+Jsuser.greetings = function(){
+    console.log("hello Js user");
+    
+}
+
+Jsuser.greetingsTwo = function(){
+    console.log(`hello Js user, ${this.name}`);// converting from string to backticks, "" -> ``. then object mein joo name hai usko reference karne ke liye hum use karenge string interpolation `${this.name}` // same object ko jab reference karna hota hai toh hum this. ka use karte hain
+    
+}
+
+console.log(Jsuser.greetings);
+console.log(Jsuser.greetingsTwo());
+
+
+// console.log(Jsuser.greetings); vs console.log(Jsuser.greetings());
+// part - 1 is completed about object.
